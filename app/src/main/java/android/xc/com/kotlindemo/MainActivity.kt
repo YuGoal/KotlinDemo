20495ad63c509b94a2053798bb43c410ce1f3cb4
YuGoal <1127caoyu@gmail.com>
2017-08-10 17:28:42 +0800

    kotlin初体验
    kotlin初体验

---
+++ app/src/main/java/android/xc/com/kotlindemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package android.xc.com.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview.setText("hello kotlin")
+    }
+}
