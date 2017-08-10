1ff9e3bc9924843cd92d513583d9c676391effb4
YuGoal <1127caoyu@gmail.com>
2017-08-10 17:34:09 +0800

    数据类型定义

--- app/src/main/java/android/xc/com/kotlindemo/MainActivity.kt
+++ app/src/main/java/android/xc/com/kotlindemo/MainActivity.kt
@@ -6,6 +6,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val a = "name"
+    val b = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
